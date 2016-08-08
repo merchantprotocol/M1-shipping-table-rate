e3e1c3512a680c30512ff9b2210b466923873496
devendra-mp <devendra@merchantprotocol.com>
2016-08-08 18:22:20 +0545

    updated uninstall.sh

---
+++ sh/mp/ShippingTablerate/uninstall.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+CONFIG_FILE="./app/etc/local.xml"
+INDEXER_FILE="shell/indexer.php"
+
+PHP_BIN=`which php`
+
+echo "Do you want to completely uninstall the extension?(y/n)"
+read UNINSTALL
+
+
+if [ "$UNINSTALL" == "y" ]; then
+
+	CWD=$(pwd)
+	
+	rm -fr $CWD/app/code/local/Innoexts/ShippingTablerate
+	rm -f $CWD/app/design/adminhtml/default/default/layout/shippingtablerate.xml
+	rm -fr $CWD/app/design/adminhtml/default/default/template/shippingtablerate/
+	rm -f $CWD/app/etc/modules/Innoexts_ShippingTablerate.xml
+	rm -f $CWD/app/locale/en_US/Innoexts_ShippingTablerate.csv
+	rm -fr $CWD/skin/adminhtml/default/default/images/shippingtablerate/
+	rm -fr $CWD/skin/adminhtml/default/default/shippingtablerate/
+	rm -f $CWD/package.xml	
+	
+	rm -fr $CWD/var/cache
+	
+	$PHP_BIN $INDEXER_FILE --reindexall	
+fi
