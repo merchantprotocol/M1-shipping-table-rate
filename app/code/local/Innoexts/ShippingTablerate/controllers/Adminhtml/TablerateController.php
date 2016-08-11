5ad52abfc9211e96ae96abd9467c3fe8bc23ac4e
Devendra Maharjan <devendram@gmail.com>
2016-08-11 14:49:38 +0545

    added license notice #4

--- app/code/local/Innoexts/ShippingTablerate/controllers/Adminhtml/TablerateController.php
+++ app/code/local/Innoexts/ShippingTablerate/controllers/Adminhtml/TablerateController.php
@@ -1,23 +1,29 @@
 <?php
 /**
- * Innoexts
+ * Merchant Protocol
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
+ * This source file is subject to the Merchant Protocol Commercial License (MPCL 1.0)
+ * that is bundled with this package in the file LICENSE.md.
  * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
+ * https://merchantprotocol.com/commercial-license/
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to license@innoexts.com so we can send you a copy immediately.
- * 
- * @category    Innoexts
- * @package     Innoexts_ShippingTablerate
- * @copyright   Copyright (c) 2011 Innoexts (http://www.innoexts.com)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * to info@merchantprotocol.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade to newer
+ * versions in the future. If you wish to customize the extension for your
+ * needs please refer to http://www.merchantprotocol.com for more information.
+ *
+ * @category    MP
+ * @package     MP_ShippingTablerate
+ * @copyright  Copyright (c) 2006-2016 Merchant Protocol LLC. and affiliates (https://merchantprotocol.com/)
+ * @license    https://merchantprotocol.com/commercial-license/  Merchant Protocol Commercial License (MPCL 1.0)
  */
-
+ 
 /**
  * Table rates controller
  * 
